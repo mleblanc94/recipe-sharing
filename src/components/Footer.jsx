bb71da8991d7f7f4815f4b0823be3f5c3ddb3b44
unknown <leblancmichael94@gmail.com>
2025-03-11 00:26:50 -0400

    adding files like header and footer with navigation to the page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'tachyons';
+
+const Footer = () => {
+  return (
+    <div className="pa3 bg-light-green tc white">
+      <p>Recipe Sharing Hub 2025 &copy;</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import Auth from '../utils/auth';
+import 'tachyons';
+import './Navigation.css';
+import  Logo  from '../assets/Logo3.png';
+
