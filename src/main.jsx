26bf2c3d975ad461f3c5a335d101411622d1bae4
unknown <leblancmichael94@gmail.com>
2025-03-13 19:40:37 -0400

    added more to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import ReactDOM from '' //ASK CHATGPT ABOUT THIS!!
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx';
+import Profile from './pages/Profile.jsx';
+import createRecipe from './pages/CreateRecipe.jsx';
 
 
 createRoot(document.getElementById('root')).render(
