09873cf7c3a23fd659c398dbd302bb9e591923f0
unknown <leblancmichael94@gmail.com>
2025-03-12 23:33:34 -0400

    started working on main.jsx to configure routing for the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from '' //ASK CHATGPT ABOUT THIS!!
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
