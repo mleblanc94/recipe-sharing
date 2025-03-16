248f83a60b297b917a078d72a33392ad20670ef4
unknown <leblancmichael94@gmail.com>
2025-03-15 21:32:29 -0400

    finished the main.jsx navigation setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,40 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx';
 import Profile from './pages/Profile.jsx';
-import createRecipe from './pages/CreateRecipe.jsx';
+import CreateRecipe from './pages/CreateRecipe.jsx';
+import Signin from './pages/Signin.jsx';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, {
+      path: '/signin',
+      element: <Signin />
+    }, {
+      path: '/signup',
+      element: <Signup />
+    }, {
+      path: '/profile',
+      element: <Profile />
+    }, {
+      path: '/create-recipe',
+      element: <CreateRecipe />
+    }, {
+      
+    }
+    ]
+  }
+])
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
 )
