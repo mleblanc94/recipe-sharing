093fdda43dbabdb69f83a32fae22577f7b4d7d86
Michael LeBlanc <mleblanc1@werfen.com>
2025-06-15 22:53:40 -0400

    added to the navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,3 +5,48 @@ import 'tachyons';
 import './Navigation.css';
 import  Logo  from '../assets/Logo3.png';
 
+const Navigation = () => {
+    const location = useLocation();
+
+    const isCurrentPage = (link) => {
+        return location.pathname === link;
+    }
+
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
+    return (
+        <nav>
+            <ul className="" >
+            <li>
+                <img src="{logo}" alt="logo" 
+                className="shadow-lg p-1 mb-1 bg-white rounded" />
+            </li>
+            <li>
+                <Link to="/" className={`near-white ${isCurrentPage('/') ? fw8 : ''}`} >
+                Home
+                </Link>
+            </li>
+            {
+                Auth.loggedIn() ? (
+                    <>
+                    <li>
+                        <Link to="/create" className={`near-white ${isCurrentPage('/create') ? 'fw8' : ''}`}>
+                        Create
+                        </Link> 
+                    </li>
+                    <li>
+                        <Link >
+                        Profile
+                        </Link>
+                    </li>
+                ) : (
+
+                )
+            }
+            </ul>
+        </nav>
+    )
+}
