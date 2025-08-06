9028d86c5cb4f02081202546cbf952e65abf1bbf
unknown <leblancmichael94@gmail.com>
2025-08-06 19:40:30 -0400

    added both the auth page, and got mostly through adding the client side mutations as well

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,53 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+    token
+    user {
+    _id
+    username
+        }
+    }
+}
+`;
+
+export const CREATE_USER = gql`
+    mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+    token
+    user {
+    _id
+    username
+    email
+    password
+        }
+    }
+}
+`;
+
+export const CREATE_RECIPE = gql`
+mutation CreateRecipe($input: RecipeInput!) {
+    createRecipe(input: $input) {
+    _id
+    title
+    description
+    creator {
+        _id
+    }
+    recipeType
+    imageName
+    }
+}
+`;
+
+export const ADD_INTERESTED_USER = gql`
+mutation addToInterestedIn($recipeId: ID!, $userId: ID!) {
+addToInterestedIn(recipeId: $recipeId, userId: $userId) {
+_id
+interestedIn {
+    _id
+        }
+    }
+}
+`;
