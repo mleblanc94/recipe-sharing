47e17d4a0f47c134c497b93d2e9c30bdb8843e62
unknown <leblancmichael94@gmail.com>
2025-08-12 16:11:03 -0400

    making changes to the backend files

---
+++ server/config/connection.js
@@ -0,0 +1,9 @@
+// Import Mongoose
+const mongoose = require('mongoose');
+
+// Connect to mongodb database 'recipesharing' - if it doesn't exist, it will create it
+const mongoDBUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipesharing';
+mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Export Connection
+module.exports = mongoose.connection; 
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require('./User'); // Imports the User model
+const Recipe = require('./Recipe'); // Imports the Recipe model
+const RecipeType = require('./RecipeType');
+
+module.exports = { User, Recipe, RecipeType }; // Exports all the modules
