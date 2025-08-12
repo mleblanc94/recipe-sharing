29114765dea929267f0876b6b7a5386d547f4fbd
unknown <leblancmichael94@gmail.com>
2025-08-12 12:09:35 -0400

    added to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -46,10 +46,85 @@ const Home = () => {
 
   if (loading) return <p>Loading page...</p>;
 
-  const projects = data?.getNotCreatedRecipes || [];
+  const recipes = data?.getNotCreatedRecipes || [];
 
-  const openRecipeDetails = (project) => {
+  const openRecipeDetails = (recipe) => {
     setSelectedRecipe(recipe);
   }
+
+  const isRecipeInterestedIn = (recipeId, interestedInID) => {
+    const userExists = interestedInID.some(user => user._id === userId);
+    if (userExists) {
+      return <label>Already chosen recipe previously</label>
+    } else {
+      return (
+        <button onClick={(e) => addInterestedIn(recipeId, e)}>
+          Interested
+        </button>
+      );
+    }
+  };
+
+  const addToFavorites = (recipe) => {
+    setFavorites([...favorites, recipe]);
+  }
+
+  const addInterested = async (recipeId, e) => {
+    e.stopPropagation();
+    try {
+      const { data } = await addInterested({
+        variables: {
+          recipeId,
+          userId,
+        },
+      });
+      console.log('User added to InterestedIn', data.addToInterestedIn);
+    } catch (error) {
+      console.error('Error adding user to interested in:', error.message);
+    }
+  };
+
+  return (
+    <div className="pa4">
+      <div>
+        <h1>Welcome to Recipe Share!</h1>
+        <p>Get ideas on the best community recipes around</p>
+        {/* Keep your intro paragraphs here */}
+      </div>
+      <h1 className="tc">Recipes</h1>
+      <div className="flex justify-center">
+        <div className="flex flex-wrap justify-between mw8">
+          {recipes.map((recipe, index) => (
+            <article
+              key={index}
+              className="br2 ba dark-gray b--black-10 mv4 w-100 w-40-l shadow-5 ma2"
+              onClick={() => openRecipeDetails(recipe)}
+              style={{ cursor: 'pointer' }}
+            >
+              <main className="pa4 black-80">
+                <h2 className="f4 fw6">{recipe.title}</h2>
+                <img
+                  src={getImageSource(recipe.imageName)}
+                  style={{ maxWidth: '250px', maxHeight: '250px' }}
+                  alt={recipe.title}
+                  className="w-100 pointer"
+                  onClick={(e) => e.stopPropagation()}
+                />
+                <p>{recipe.description}</p>
+              </main>
+            </article>
+          ))}
+        </div>
+      </div>
+
+      {selectedRecipe && (
+          <RecipeDetails
+            recipe={selectedRecipe}
+            addToFavorites={addToFavorites}
+            closeModal={() => setSelectedRecipe(null)}
+          />
+      )}
+    </div>
+  )
 
 }
