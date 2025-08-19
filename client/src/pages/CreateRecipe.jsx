605e778904cc5806e5d7e6ccb03f8cfce6652ab2
unknown <leblancmichael94@gmail.com>
2025-08-19 11:47:24 -0400

    added further to create project react page

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -56,7 +56,36 @@ const CreateRecipe = () => {
 
       // Push the user to the profile page to show them it appears there
       window.location.assign('/profile');
+    } catch (error) {
+      // Show error
+      console.error('Error creating project:', error.message);
     }
   }
+
+  const images = [
+    { name: 'image1', src: image1 },
+    { name: 'image2', src: image2 },
+    { name: 'image3', src: image3 },
+    { name: 'image4', src: image4 },
+    { name: 'image5', src: image5 },
+    { name: 'image6', src: image6 },
+  ];
+
+  return (
+    <div className="pa4" style={{ maxWidth: '600px', margin: '0 auto' }}>
+      <article className="br2 ba dark-gray b--black-10 mv4 shadow-5">
+        <main className="pa4 black-80">
+          <form className="measure" onSubmit={handleSubmit}>
+            <fieldset id="createRecipe" className="ba b--transparent ph0 mh0">
+              <legend className="f4 fw6 ph0 mh0">Create a Recipe</legend>
+              <div className="mv3">
+                <label className="db fw6 1h-copy f6"></label>
+              </div>
+            </fieldset>
+          </form>
+        </main>
+      </article>
+    </div>
+  )
 
 }
