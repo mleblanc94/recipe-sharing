05aa5cc02e23a0820996421e86ad5b2bf1d7a8bf
unknown <leblancmichael94@gmail.com>
2025-09-15 12:12:19 -0400

    added more to tests

--- cypress/e2e/01_login.cy.js
+++ cypress/e2e/01_login.cy.js
@@ -2,8 +2,7 @@ import '../support/commands';
 
 describe('Page loads when visitng the URL and the user can signin', () => {
   it('passes', () => {
-    // Reference login function within the commands.js file and log into application
+    // Call login function within the commands.js file and log into application
     cy.userLogin();
-  }
-  )
+  })
 })
